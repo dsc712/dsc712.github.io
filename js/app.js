0a4c9df3e05ec9ac2cd0efa463217bcc9927054f
I524754 <devyendu.shekhar.choudhary@sap.com>
2021-05-05 20:56:36 +0530

    add stack trace

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
-    console.warn("demo warning");
-    // generate console errors
-    this.getErrors();
-    null.firstname;
-    alert("Link is Clicked");
     
+    myFunc();
+    // generate console errors
+    // this.getErrors();
+
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -33,3 +31,40 @@ $(document).ready(function(){
 }
 
 );
+
+function myFunc() {
+  b();
+}
+function b() {
+// generate console errors
+this.getErrors();
+}
+
+errorObj = {};
+// window.addEventListener('error', function(e) {
+//   console.log("evtObj", e);
+// })
+
+window.onerror = function(msg, url, lineNo, columnNo, error) {
+  console.log("erroOBj", error);
+  errorObj = {
+    'msg': msg,
+    'url': url,
+    'lineNo': lineNo,
+    'columnNo': columnNo,
+    'Stack Trace: ': error.stack.split("\n")
+  };
+  console.log("UPDATED SWA", errorObj);
+};
+
+// window.onerror = function errorhandler(msg, url, ln, colno, err) {
+//   try {
+//     var trace = printStackTrace({e: msg});
+//     var traceMsg = printStackTrace({e: msg }).join("\n");
+//     console.log(traceMsg);
+//     console.log(trace);
+//   } catch (error) { // do nothing
+//     alert("Error:" + msg);
+//     return true;
+//   }
+// }
